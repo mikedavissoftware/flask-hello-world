3ad02cb0edba404d72d393ea19c18212e32d7701
mikedavissoftware <mikedavissoftware@gmail.com>
2026-04-07 21:44:51 -0600

    db_test route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Mike Davis in 3308!'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://render_tutorial_db_cnqp_user:SJybzxIsRC5XycPzOK1MG2oFYz8B0UMH@dpg-d7ast0cvjg8s73emsjmg-a.virginia-postgres.render.com/render_tutorial_db_cnqp")
+    conn.close()
+    return "Database Connection Successful"
